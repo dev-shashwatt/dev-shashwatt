f4f0d9cff2b707b8a85affa191f6c2d4de69226b
shashwat <shashwatmukul2007@gmail.com>
2023-07-30 23:44:15 +0530

    Create script.js

---
+++ script/script.js
@@ -0,0 +1,17 @@
+.swiper {
+    width: 100%;
+    height: 100%;
+}
+
+.swiper-slide {
+    text-align: center;
+    font-size: 18px;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+}
+
+.swiper-slide img {
+    display: block;
+    object-fit: cover;
+}
